2ba5307fa6de1fd411ea1c692a276ca7619b6f88
asanibrahim <asanib98@gmail.com>
2024-05-22 21:41:15 +0530

    method

--- myapp/src/myapp/Asan.java
+++ myapp/src/myapp/Asan.java
@@ -2,11 +2,8 @@ package myapp;
 
 public class Asan {
 
-	public static void add() {
-		int a = 10;
-		int b = 20;
-		
-		int c = a + b ;
-		System.out.println(c);
+	public static int add(int a , int b) {
+		int c = a +b ;
+		return  c;
 	}
 }
