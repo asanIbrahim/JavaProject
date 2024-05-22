c4ec7c5809e1b3fbce08750ef40820f61e30fa18
asanibrahim <asanib98@gmail.com>
2024-05-22 21:41:32 +0530

    add main

--- myapp/src/myapp/Main.java
+++ myapp/src/myapp/Main.java
@@ -6,11 +6,11 @@ public class Main {
 	String address = "";
 	public static void main(String[] args) {
 	Main customer = new Main();
-	
-	
+	System.out.println(Asan.add(12,18));
 	customer.employeeName = "Asan";
 	customer.address = "plot 26, Govarthan Garder";
 	
 	System.out.println(customer.employeeName);
+	
 }
 }
