540068b9ccc4fd83de050cc7679c954d61bc7551
asanibrahim <asanib98@gmail.com>
2024-05-22 15:22:14 +0530

    initial commit.

---
+++ myapp/src/myapp/Main.java
@@ -0,0 +1,13 @@
+package myapp;
+
+public class Main {
+	String employeeName  = "";
+	int age;
+	public static void main(String[] args) {
+	Main address = new Main();
+	
+	address.employeeName = "Asan";
+	
+	System.out.println(address.employeeName);
+}
+}
